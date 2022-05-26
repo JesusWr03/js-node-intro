cea6de9708eab0e1377f807b1628407e55715652
Jesus <payjsms@outlook.com>
2022-05-26 12:59:16 -0500

    add first exercise

---
+++ exercise-1.js
@@ -0,0 +1,12 @@
+const profile = {
+    name: "jesus",
+    age: 19,
+    dev: true,
+    born: 2003,
+    favBook: {
+        title: "El Principito",
+        author: "Antoine de Saint-Exupéry",
+        date: 1943,
+        url: "https://www.amazon.com/-/es/Antoine-Saint-exupery/dp/8498381495"
+    }
+}
