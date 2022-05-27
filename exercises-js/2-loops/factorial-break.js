1aed4f908ecf503682c2446d1ebd5470fb95e652
Jesus <payjsms@outlook.com>
2022-05-27 18:54:55 -0500

    more exercise

---
+++ exercises-js/2-loops/factorial-break.js
@@ -0,0 +1,11 @@
+let fact = 1 
+let i = 0;
+
+while(true){
+  i++
+  fact *= i
+  if(i >= 10){
+      break;
+  }
+}
+console.log(fact)
---
+++ exercises-js/2-loops/factorial-while.js
@@ -0,0 +1,8 @@
+let fact = 1
+let i = 0;
+
+while(i < 10){
+  i++
+  fact *= i
+}
+console.log(fact)
