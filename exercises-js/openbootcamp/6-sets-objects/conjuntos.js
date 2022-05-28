4e149b5c08e3162265a2f317194cd7d210ee7edf
Jesus <payjsms@outlook.com>
2022-05-28 18:54:53 -0500

    s

---
+++ exercises-js/openbootcamp/6-sets-objects/conjuntos.js
@@ -0,0 +1,8 @@
+const listaAr = ["php", "python", "java", "c++", "rust"];
+const lenguajeSet = new Set(listaAr);
+
+// añadiendo lenguaje que ya existe en el Set
+lenguajeSet.add("php");
+
+// añadiendo javascript
+lenguajeSet.add('javascript')
