14e9f823304f19dcac01ea93770b88b04c8c9668
Jesus <payjsms@outlook.com>
2022-05-27 23:19:05 -0500

    fix

--- exercises-js/openbootcamp/4-numbers.js
+++ exercises-js/openbootcamp/4-numbers.js
@@ -3,7 +3,7 @@ const myHeightMt = 1.83;
 
 const myWeight = 70.3;
 
-const roundedHeight = myHeightMt.toFixed(0)
-const roundedWeight = myWeight.toFixed(0)
+const roundedHeight = Math.ceil(myHeightCm)
+const roundedWeight = Math.floor(myWeight)
 
 const maxValueJs = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
