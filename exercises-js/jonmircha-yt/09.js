dc5339e5b661183859ff802a983e398cbee92b18
Jesus <payjsms@outlook.com>
2022-05-28 02:03:42 -0500

    done

---
+++ exercises-js/jonmircha-yt/09.js
@@ -0,0 +1,13 @@
+//Programa una función que obtenga un numero aleatorio entre 501 y 600.
+
+const randomNum = (min = 0, max = 1) => {
+  if (typeof min !== "number" && typeof max !== "number")
+    console.warn("Ingresa solo numeros");
+  else if (min >= max) {
+    console.warn("Debes especificar un minimo y maximo valido");
+  } else {
+    console.log(Math.floor(Math.random() * (max - min)) + min);
+  }
+};
+
+randomNum(5, 2);
