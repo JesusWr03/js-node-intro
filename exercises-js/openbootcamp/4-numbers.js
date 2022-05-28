a56be2d38b2e267d885d37b6b1209e106db3511d
Jesus <payjsms@outlook.com>
2022-05-27 23:17:05 -0500

    d

---
+++ exercises-js/openbootcamp/4-numbers.js
@@ -0,0 +1,9 @@
+const myHeightCm = 183;
+const myHeightMt = 1.83;
+
+const myWeight = 70.3;
+
+const roundedHeight = myHeightMt.toFixed(0)
+const roundedWeight = myWeight.toFixed(0)
+
+const maxValueJs = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
